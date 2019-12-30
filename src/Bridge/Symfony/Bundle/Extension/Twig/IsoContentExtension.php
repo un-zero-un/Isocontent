391a19e8c7330fbb6b37320f2836104b55e61a8e
axel2309 <34721346+axel2309@users.noreply.github.com>
2019-12-30 14:15:46 +0100

    adding Twig extension to render AST in a html template

---
+++ src/Bridge/Symfony/Bundle/Extension/Twig/IsoContentExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Isocontent\Bridge\Symfony\Bundle\Extension\Twig;
+
+use Isocontent\Renderer\HTMLRenderer;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Twig extension for displaying IsoContent
+ *
+ * @author Yohan Giarelli <yohan@un-zero-un.fr>
+ */
+class IsoContentExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('display_isocontent', [$this, 'displayIsoContent']),
+        ];
+    }
+
+    public function displayIsoContent($ast): string
+    {
+        return (new HTMLRenderer)->render($ast);
+    }
+}
