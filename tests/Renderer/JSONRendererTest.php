572d360720eddf5bce646fc4ec3d91685d111835
axel2309 <34721346+axel2309@users.noreply.github.com>
2019-12-31 16:27:01 +0100

    added more JsonRenderer test

--- tests/Renderer/JSONRendererTest.php
+++ tests/Renderer/JSONRendererTest.php
@@ -58,6 +58,49 @@ class JSONRendererTest extends TestCase
                 '[{"type":"block","block_type":"inline_text","children":[{"type":"text","value":"foobar"}]},' .
                 '{"type":"block","block_type":"strong","children":[{"type":"text","value":"bazqux"}]}]',
             ],
+            [
+                NodeList::fromArray([
+                    BlockNode::fromBlockType('title', ['level' => 4], NodeList::fromArray([
+                        TextNode::fromText('level 4 title'),
+                    ])),
+                    BlockNode::fromBlockType('quote', [], NodeList::fromArray([
+                        TextNode::fromText('Quoted item'),
+
+                    ])),
+
+                    BlockNode::fromBlockType('title', ['level' => 5], NodeList::fromArray([
+                        TextNode::fromText('level 5 title'),
+                    ])),
+                    TextNode::fromText('followed by an'),
+                    BlockNode::fromBlockType('new_line', [], null),
+                    BlockNode::fromBlockType('emphasis', [], NodeList::fromArray([
+                        TextNode::fromText('emphasis'),
+                    ])),
+                    TextNode::fromText('and'),
+                    BlockNode::fromBlockType('strong', [], NodeList::fromArray([
+                        TextNode::fromText('Strong text'),
+                    ])),
+                    TextNode::fromText('followed by'),
+                    BlockNode::fromBlockType('list', ['ordered' => false], NodeList::fromArray([
+                        BlockNode::fromBlockType('list_item', ['ordered' => false], NodeList::fromArray([
+                            TextNode::fromText('a list'),
+                        ])),BlockNode::fromBlockType('list_item', ['ordered' => false], NodeList::fromArray([
+                            TextNode::fromText('of item'),
+                        ]))
+                    ])),
+                ]),
+                '[{"level":4,"type":"block","block_type":"title","children":[{"type":"text","value":"level 4 title"}]},'.
+                '{"type":"block","block_type":"quote","children":[{"type":"text","value":"Quoted item"}]},'.
+                '{"level":5,"type":"block","block_type":"title","children":[{"type":"text","value":"level 5 title"}]},'.
+                '{"type":"text","value":"followed by an"},'.
+                '{"type":"block","block_type":"new_line"},'.
+                '{"type":"block","block_type":"emphasis","children":[{"type":"text","value":"emphasis"}]},'.
+                '{"type":"text","value":"and"},'.
+                '{"type":"block","block_type":"strong","children":[{"type":"text","value":"Strong text"}]},'.
+                '{"type":"text","value":"followed by"},'.
+                '{"ordered":false,"type":"block","block_type":"list","children":[{"ordered":false,"type":"block","block_type":"list_item","children":[{"type":"text","value":"a list"}]},' .
+                '{"ordered":false,"type":"block","block_type":"list_item","children":[{"type":"text","value":"of item"}]}]}]',
+            ],
         ];
     }
 }
